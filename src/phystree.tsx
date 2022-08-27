b5492a5c06729db1b4b7ff4a46067e39c669dc82
Fábio Machado de Oliveira <machado2@gmail.com>
2022-08-26 21:31:47 -0300

    Textures

--- src/phystree.tsx
+++ src/phystree.tsx
@@ -1,12 +1,20 @@
 
 
-import { Cone, Cylinder, OrbitControls, Plane } from '@react-three/drei';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Cone, Cylinder, OrbitControls, Plane, Stars, useTexture } from '@react-three/drei';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { Physics, RigidBody, Vector3Array } from '@react-three/rapier';
 import niceColors from 'nice-color-palettes';
 import { Perf, usePerf } from 'r3f-perf';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { MeshPhysicalMaterial, MirroredRepeatWrapping, RepeatWrapping, Texture, TextureLoader } from 'three';
 import * as uuid from 'uuid';
+import groundTextureUrl from './assets/TexturesCom_Grass0095_M.jpg'
+
+import woodAlbedoUrl from './assets/TexturesCom_Wood_BarkWillow_0.3x0.6_1K_albedo.jpg'
+import woodAOurl from './assets/TexturesCom_Wood_BarkWillow_0.3x0.6_1K_ao.jpg'
+import woodHeightUrl from './assets/TexturesCom_Wood_BarkWillow_0.3x0.6_1K_height.jpg'
+import woodNormalUrl from './assets/TexturesCom_Wood_BarkWillow_0.3x0.6_1K_normal.jpg'
+import woodRoughnessUrl from './assets/TexturesCom_Wood_BarkWillow_0.3x0.6_1K_roughness.jpg'
 
 const halfpi = Math.PI / 2
 const angsize = Math.PI / 1.5
@@ -29,20 +37,26 @@ function Branch(props: any) {
     const radius2 = 0.05 / (props.level + 1);
     const color = useMemo(() => Math.floor(Math.random() * 0xFFFFFF), [idmemo]);
 
-    const children = [];
-    if (props.level < 5) {
-        for (let i = 0; i < 3; i++) {
-            children.push(<Branch key={i} matprop={props.matprop} position={[0, len / 2, 0]} level={props.level + 1} color={color} />);
-        }
-    }
+    const material = level == 5 ? new MeshPhysicalMaterial({ color: 'green' }) : props.matprop
+
+    const numChildren = [2, 3, 4, 4, 10, 0][props.level]
+
+    const children = [...Array(numChildren).keys()]
+        .map((_, i) =>
+            <Branch
+                key={i}
+                matprop={props.matprop}
+                position={[0, len / 2, 0]}
+                level={props.level + 1}
+                material={props.material}
+                color={color} />);
+
     return <>
         <mesh {...props} rotation-x={rotx} rotation-y={roty} rotation-z={rotz} scale="1" ref={mesh}>
             <group>
                 <mesh position={[0, len / 2, 0]} castShadow>
                     <RigidBody type="fixed">
-                        <Cylinder args={[radius2, radius, len]}>
-                            <meshPhysicalMaterial color={props.color} />
-                        </Cylinder>
+                        <Cylinder args={[radius2, radius, len]} material={material} />
                     </RigidBody>
                     {children}
                 </mesh>
@@ -59,7 +73,6 @@ function Snowflake(props: { position: Vector3Array }) {
     const rbottom = 0.05
     const vel: Vector3Array = [rndz(2), rndz(2), rndz(2)]
     const avel: Vector3Array = [rndz(2), rndz(2), rndz(2)]
-    /// const material = <meshLambertMaterial transparent opacity={0.7} color={niceColors[0][Math.floor(Math.random() * 5)]} />
     const material = <meshPhysicalMaterial transmission={0.9} ior={0.5} color={niceColors[0][Math.floor(Math.random() * 5)]} />
     return <RigidBody position={props.position} linearDamping={2} linearVelocity={vel} angularVelocity={avel}>
         {/*@ts-ignore*/}
@@ -79,7 +92,6 @@ function Snowflake(props: { position: Vector3Array }) {
 
 let lastfps: number[] = []
 const minsnow: number = 50
-const maxsnow: number = 1000
 
 function Snow(): JSX.Element {
     const [spheres, setSpheres] = useState<JSX.Element[]>([])
@@ -106,20 +118,44 @@ function Snow(): JSX.Element {
     return <>{spheres}</>
 }
 
-export function Physicstree() {
-    /*
+function _SnowFlakeDebug() {
     return <Canvas>
         <Physics>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <pointLight position={[10, 10, -10]} />
             <group scale={10}>
-                <Snowflake />
+                <Snowflake position={[0, 0, 0]} />
             </group>
         </Physics>
         <OrbitControls />
     </Canvas>
-*/
+}
+
+function WoodTree() {
+    const woodmaps: object = useTexture({
+        map: woodAlbedoUrl,
+        displacementMap: woodHeightUrl,
+        normalMap: woodNormalUrl,
+        roughnessMap: woodRoughnessUrl,
+        aoMap: woodAOurl,
+    })
+    for (let txt of Object.values(woodmaps) as Texture[]) {
+        txt.repeat.set(1, 1)
+        txt.wrapS = MirroredRepeatWrapping
+        txt.wrapT = MirroredRepeatWrapping
+    }
+    const wood = new MeshPhysicalMaterial({ ...woodmaps, displacementScale: 0.02 })
+    return <Branch position={[0, -1, 0]} level={1} matprop={wood} />
+}
+
+export function Physicstree() {
+
+    const colorMap = useLoader(TextureLoader, groundTextureUrl)
+    colorMap.repeat.set(200, 200)
+    colorMap.wrapS = MirroredRepeatWrapping
+    colorMap.wrapT = MirroredRepeatWrapping
+
     return <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
@@ -127,12 +163,13 @@ export function Physicstree() {
         <Physics gravity={[0, -1, 0]}>
             <RigidBody>
                 <Plane args={[1000, 1000]} position-y={-1} rotation-x={-halfpi}>
-                    <meshPhysicalMaterial color="red" />
+                    <meshPhysicalMaterial map={colorMap} />
                 </Plane>
             </RigidBody>
-            <Branch position={[0, -1, 0]} level={1} />
+            <WoodTree />
             <Snow />
-            <Perf />
+            <Perf headless />
+            <Stars />
         </Physics>
         <OrbitControls />
     </Canvas >;
